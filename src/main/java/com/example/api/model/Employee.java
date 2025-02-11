2d645a71144486f1c0d4125b77dd2df27d7755c3
Maëlise Castel <maelise.castel@flexicty.energy>
2025-02-11 22:48:00 +0100

    :bug: tada

--- src/main/java/com/example/api/model/Employee.java
+++ src/main/java/com/example/api/model/Employee.java
@@ -1,12 +1,12 @@
 package com.example.api.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Data;
 
 @Data
@@ -24,8 +24,10 @@ public class Employee {
     @Column(name="last_name")
     private String lastName;
 
+    @Column
     private String mail;
 
+    @Column
     private String password;
 
 }
--- src/test/java/com/example/api/EmployeeControllerTest.java
+++ src/test/java/com/example/api/EmployeeControllerTest.java
@@ -10,23 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
-
-import com.example.api.controller.EmployeeController;
-import com.example.api.service.EmployeeService;
 
 
-
-@WebMvcTest(controllers = EmployeeController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EmployeeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
-    private EmployeeService employeeService;
 
     @Test
     public void testGetEmployees() throws Exception {
