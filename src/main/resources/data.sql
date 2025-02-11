46c7a65ab6c088e8f1d9e83ae37333eab75d6389
Mithian Acuna <59997173+Butterpod@users.noreply.github.com>
2025-02-11 01:29:28 +0100

    Update data.sql
    Correction du ;

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,5 +11,4 @@ CREATE TABLE employees (
 INSERT INTO employees (first_name, last_name, mail, password) VALUES
   ('Lulu', 'SUPPORT', 'laurentgina@mail.com', 'lulu'),
   ('Vex', 'MID', 'sophiefoncek@mail.com', 'vex'),
-  ('Poppy', 'TOP', 'agathefeeling@mail.com', 'poppy')
-  ('Ziggs', 'KOBE', 'agathefeeling@mail.com', 'ziggs');
+  ('Poppy', 'TOP', 'agathefeeling@mail.com', 'poppy');
